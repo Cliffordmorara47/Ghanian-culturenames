4dd6235dadbd31363fb08bde55075e44138dc03a
cliffordmorara <cliffordmorara45@gmail.com>
2020-12-13 20:46:05 +0300

    added javascript for the form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+function validate(){
+    var details = document.getElementsByName("myForm")
+    if (document.myForm.year.value === "" && document.myForm.year.length != 4){
+        alert("Please provide a valid year!");
+        document.myForm.year.focus();
+        return false;
+    }
+    else if(document.myForm.month.value === "" || document.myForm.year.value.length >12 || document.myForm.value <=0 || isNaN(document.myForm.month.value)){
+        alert("Please provide a valid month!");
+        document.myForm.month.focus();
+        return false
+    }
+    else if ()
+}
 function dayOfTheWeek() {
     year = document.getElementById("year").nodeValue;
     CC = parseInt(year.substring(0,2));
