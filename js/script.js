7812368c5500bbeaec1969019c3c1c1fb23579ba
cliffordmorara <cliffordmorara45@gmail.com>
2020-12-13 21:37:05 +0300

    styled the form using javscript

--- js/script.js
+++ js/script.js
@@ -5,12 +5,23 @@ function validate(){
         document.myForm.year.focus();
         return false;
     }
-    else if(document.myForm.month.value === "" || document.myForm.year.value.length >12 || document.myForm.value <=0 || isNaN(document.myForm.month.value)){
+    else if(document.myForm.month.value === "" && document.myForm.year.value.length >12 && document.myForm.value <=0 || isNaN(document.myForm.month.value)){
         alert("Please provide a valid month!");
         document.myForm.month.focus();
-        return false
+        return false;
     }
-    else if ()
+    else if (document.myForm.date.value === "" && document.myForm.date.value >31 && document.myForm.date.value <=0){
+        alert("Please provide a valid date!");
+        document.myForm.day.focus();
+        return false;
+    }
+    else if(genders[checked === false] && genders[1].checked === false){
+        alert("You must select male or female");
+        return false;
+    } 
+    else{
+        return true;
+    }
 }
 function dayOfTheWeek() {
     year = document.getElementById("year").nodeValue;
